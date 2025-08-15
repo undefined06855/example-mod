a2e7eb437818d113e998ff121ddbae3859d16599
undefined06855 <98057141+undefined06855@users.noreply.github.com>
2025-08-15 18:08:23 +0100

    main.cpp tabs -> spaces

--- src/main.cpp
+++ src/main.cpp
@@ -26,75 +26,75 @@ using namespace geode::prelude;
  */
 #include <Geode/modify/MenuLayer.hpp>
 class $modify(MyMenuLayer, MenuLayer) {
-	/**
-	 * Typically classes in GD are initialized using the `init` function, (though not always!),
-	 * so here we use it to add our own button to the bottom menu.
-	 *
-	 * Note that for all hooks, your signature has to *match exactly*,
-	 * `void init()` would not place a hook!
-	*/
-	bool init() {
-		/**
-		 * We call the original init function so that the
-		 * original class is properly initialized.
-		 */
-		if (!MenuLayer::init()) {
-			return false;
-		}
+    /**
+     * Typically classes in GD are initialized using the `init` function, (though not always!),
+     * so here we use it to add our own button to the bottom menu.
+     *
+     * Note that for all hooks, your signature has to *match exactly*,
+     * `void init()` would not place a hook!
+    */
+    bool init() {
+        /**
+         * We call the original init function so that the
+         * original class is properly initialized.
+         */
+        if (!MenuLayer::init()) {
+            return false;
+        }
 
-		/**
-		 * You can use methods from the `geode::log` namespace to log messages to the console,
-		 * being useful for debugging and such. See this page for more info about logging:
-		 * https://docs.geode-sdk.org/tutorials/logging
-		*/
-		log::debug("Hello from my MenuLayer::init hook! This layer has {} children.", this->getChildrenCount());
+        /**
+         * You can use methods from the `geode::log` namespace to log messages to the console,
+         * being useful for debugging and such. See this page for more info about logging:
+         * https://docs.geode-sdk.org/tutorials/logging
+        */
+        log::debug("Hello from my MenuLayer::init hook! This layer has {} children.", this->getChildrenCount());
 
-		/**
-		 * See this page for more info about buttons
-		 * https://docs.geode-sdk.org/tutorials/buttons
-		*/
-		auto myButton = CCMenuItemSpriteExtra::create(
-			CCSprite::createWithSpriteFrameName("GJ_likeBtn_001.png"),
-			this,
-			/**
-			 * Here we use the name we set earlier for our modify class.
-			*/
-			menu_selector(MyMenuLayer::onMyButton)
-		);
+        /**
+         * See this page for more info about buttons
+         * https://docs.geode-sdk.org/tutorials/buttons
+        */
+        auto myButton = CCMenuItemSpriteExtra::create(
+            CCSprite::createWithSpriteFrameName("GJ_likeBtn_001.png"),
+            this,
+            /**
+             * Here we use the name we set earlier for our modify class.
+            */
+            menu_selector(MyMenuLayer::onMyButton)
+        );
 
-		/**
-		 * Here we access the `bottom-menu` node by its ID, and add our button to it.
-		 * Node IDs are a Geode feature, see this page for more info about it:
-		 * https://docs.geode-sdk.org/tutorials/nodetree
-		*/
-		auto menu = this->getChildByID("bottom-menu");
-		menu->addChild(myButton);
+        /**
+         * Here we access the `bottom-menu` node by its ID, and add our button to it.
+         * Node IDs are a Geode feature, see this page for more info about it:
+         * https://docs.geode-sdk.org/tutorials/nodetree
+        */
+        auto menu = this->getChildByID("bottom-menu");
+        menu->addChild(myButton);
 
-		/**
-		 * The `_spr` string literal operator just prefixes the string with
-		 * your mod id followed by a slash. This is good practice for setting your own node ids.
-		*/
-		myButton->setID("my-button"_spr);
+        /**
+         * The `_spr` string literal operator just prefixes the string with
+         * your mod id followed by a slash. This is good practice for setting your own node ids.
+        */
+        myButton->setID("my-button"_spr);
 
-		/**
-		 * We update the layout of the menu to ensure that our button is properly placed.
-		 * This is yet another Geode feature, see this page for more info about it:
-		 * https://docs.geode-sdk.org/tutorials/layouts
-		*/
-		menu->updateLayout();
+        /**
+         * We update the layout of the menu to ensure that our button is properly placed.
+         * This is yet another Geode feature, see this page for more info about it:
+         * https://docs.geode-sdk.org/tutorials/layouts
+        */
+        menu->updateLayout();
 
-		/**
-		 * We return `true` to indicate that the class was properly initialized.
-		 */
-		return true;
-	}
+        /**
+         * We return `true` to indicate that the class was properly initialized.
+         */
+        return true;
+    }
 
-	/**
-	 * This is the callback function for the button we created earlier.
-	 * The signature for button callbacks must always be the same,
-	 * return type `void` and taking a `CCObject*`.
-	*/
-	void onMyButton(CCObject*) {
-		FLAlertLayer::create("Geode", "Hello from my custom mod!", "OK")->show();
-	}
+    /**
+     * This is the callback function for the button we created earlier.
+     * The signature for button callbacks must always be the same,
+     * return type `void` and taking a `CCObject*`.
+    */
+    void onMyButton(CCObject*) {
+        FLAlertLayer::create("Geode", "Hello from my custom mod!", "OK")->show();
+    }
 };
